6fed625195893ed276447be624964fbce7a95df7
Zakaria Makrelouf <z.makrelouf@gmail.com>
2017-10-16 13:03:52 +0100

    [REF]stock_auto_move: transfer auto move after do_transfer is done

--- stock_auto_move/models/stock_picking.py
+++ stock_auto_move/models/stock_picking.py
@@ -9,27 +9,58 @@ class StockPicking(models.Model):
 
     _inherit = 'stock.picking'
 
-    @api.model
-    def _transfer_pickings_with_auto_move(self, auto_moves_by_pickings):
+    @api.multi
+    def do_transfer(self):
+        res = super(StockPicking, self).do_transfer()
+        self._transfer_pickings_with_auto_move()
+        return res
+
+    @api.multi
+    def _transfer_pickings_with_auto_move(self):
         """This function is meant to simulate what a user would normally
         transfer a picking from the user interface either partial processing
         or full processing.
-        @params auto_moves_by_pickings: dict of moves grouped by pickings
-        {stock.picking(id): stock.move(id1, id2, id3 ...), ...}
         """
-        for picking in auto_moves_by_pickings:
-            if len(picking.move_lines) != len(auto_moves_by_pickings[picking]):
-                # Create a back order for remaning moves
-                backorder_moves = \
-                    picking.move_lines - auto_moves_by_pickings[picking]
-                self._create_backorder(
-                    picking=picking, backorder_moves=backorder_moves)
+        for picking in self:
+            auto_moves = self._get_auto_moves(picking)
+            auto_moves_by_pickings = self._get_auto_moves_by_pickings(auto_moves)
+            for auto_picking in auto_moves_by_pickings:
+                if len(auto_picking.move_lines) != len(
+                        auto_moves_by_pickings[auto_picking]):
+                    # Create a back order for remaning moves
+                    backorder_moves = \
+                        auto_picking.move_lines - \
+                        auto_moves_by_pickings[auto_picking]
+                    self._create_backorder(
+                        picking=auto_picking, backorder_moves=backorder_moves)
 
-            # Create immediate transfer wizard so it will fill the qty_done
-            # on the auto move linked operation
-            picking.do_prepare_partial()
-            wizard = self.env['stock.immediate.transfer'].create(
-                {'pick_id': picking.id})
-            wizard.process()
+                # Create immediate transfer wizard so it will fill the qty_done
+                # on the auto move linked operation
+                auto_picking.do_prepare_partial()
+                wizard = self.env['stock.immediate.transfer'].create(
+                    {'pick_id': auto_picking.id})
+                wizard.process()
 
         return
+
+    @api.model
+    def _get_auto_moves(self, picking):
+        dest_moves = picking.move_lines.mapped('move_dest_id')
+
+        return dest_moves.filtered(
+            lambda m: m.state == 'assigned' and m.auto_move)
+
+    @api.model
+    def _get_auto_moves_by_pickings(self, auto_moves):
+        """ Group moves by picking.
+        @param auto_moves: stock.move data set
+        @return dict dict of moves grouped by pickings
+        {stock.picking(id): stock.move(id1, id2, id3 ...), ...}
+        """
+        auto_moves_by_pickings = dict()
+        for move in auto_moves:
+            if move.picking_id in auto_moves_by_pickings:
+                auto_moves_by_pickings[move.picking_id] |= move
+            else:
+                auto_moves_by_pickings.update({move.picking_id: move})
+        return auto_moves_by_pickings
